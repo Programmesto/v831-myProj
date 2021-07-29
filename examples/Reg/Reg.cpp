975c842a477a1a32eac835de60d60a0f3566b264
student4 <student4@mir.dev>
2021-07-29 11:49:41 +0300

    (uint32_t*)(gp_paddr)+buf1_offset)

--- examples/Reg/Reg.cpp
+++ examples/Reg/Reg.cpp
@@ -31,8 +31,9 @@ void myProgram() {
 	
 	
 	uint32_t data[5] = {1, 2, 3, 4, 5};
-	sunxi_ion_loadin((uint32_t*)data, sizeof(data), (uint32_t)(gp_paddr)+buf1_offset);
 	printf("\n%p\n", (uint32_t*)(gp_paddr)+buf1_offset);
+	sunxi_ion_loadin((uint32_t*)data, sizeof(data), (uint32_t)(gp_paddr)+buf1_offset);
+	
 	
 	
 }
