33606fe2fce547372bfd4c33439ff6e1e76c006f
student4 <student4@mir.dev>
2021-07-29 11:05:40 +0300

    uint32_t* gp_vaddr Reg

--- examples/Reg/Reg.cpp
+++ examples/Reg/Reg.cpp
@@ -20,7 +20,7 @@ void myProgram() {
 	sunxi_ion_alloc_open();
 	sunxi_ion_alloc_palloc(0x80000,&gp_vaddr,&gp_paddr);
 	
-	printf(gp_vaddr);
+	printf((uint32_t*)gp_vaddr);
 	
 	// uint32_t data[1] = {5}
 	// sunxi_ion_loadin((uint32_t*)data, sizeof(data), (uint32_t)(gp_paddr)+buf1_offset);
