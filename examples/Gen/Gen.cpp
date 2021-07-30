4d9324f252a465fa2358dabc5f789da4bc267dd9
student4 <student4@mir.dev>
2021-07-30 12:33:51 +0300

    Gen added

---
+++ examples/Gen/Gen.cpp
@@ -0,0 +1,68 @@
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include "nna_hw.h"
+#include "nna_interface.h"
+#include "nna_config.h"
+#include "ion_alloc.h"
+
+
+#include "const.h"
+
+static void* gp_vaddr;
+static void* gp_paddr;
+
+void myProgram() {
+	printf("Lets goooo!\n");
+	sunxi_ion_alloc_open();
+	sunxi_ion_alloc_palloc(0x80000, &gp_vaddr, &gp_paddr);
+	
+	uint32_t  data_offset = 0x00000;
+	sunxi_ion_loadin( resnet_v1_50_conv1_weights_ARR , sizeof( resnet_v1_50_conv1_weights_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_conv1_BatchNorm_gamma_ARR , sizeof( resnet_v1_50_conv1_BatchNorm_gamma_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_conv1_BatchNorm_beta_ARR , sizeof( resnet_v1_50_conv1_BatchNorm_beta_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_conv1_BatchNorm_moving_mean_ARR , sizeof( resnet_v1_50_conv1_BatchNorm_moving_mean_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_conv1_BatchNorm_moving_variance_ARR , sizeof( resnet_v1_50_conv1_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( alu_mul_resnet_v1_50_conv1_BatchNorm_moving_variance_ARR , sizeof( alu_mul_resnet_v1_50_conv1_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_weights_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_weights_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_gamma_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_gamma_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_beta_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_beta_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_moving_mean_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_moving_mean_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_moving_variance_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_moving_variance_ARR , sizeof( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_shortcut_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_weights_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_weights_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_gamma_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_gamma_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_beta_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_beta_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_moving_mean_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_moving_mean_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_moving_variance_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_moving_variance_ARR , sizeof( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_conv1_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_weights_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_weights_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_gamma_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_gamma_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_beta_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_beta_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_moving_mean_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_moving_mean_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_moving_variance_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_moving_variance_ARR , sizeof( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_conv2_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_weights_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_weights_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_gamma_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_gamma_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_beta_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_beta_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_moving_mean_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_moving_mean_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_moving_variance_ARR , sizeof( resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadin( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_moving_variance_ARR , sizeof( alu_mul_resnet_v1_50_block1_unit_1_bottleneck_v1_conv3_BatchNorm_moving_variance_ARR ), (uint32_t)(gp_paddr)+data_offset);
+}
+int main(int argc, char** argv) {
+	nna_configure(nna_cmd_clk, 400);
+	nna_on();
+	void* r = xreg_open();
+	if (r) {
+		nna_reset();
+		myProgram();
+		xreg_close();
+	}
+	nna_off();
+}
+
--- examples/Reg/Reg.cpp
+++ examples/Reg/Reg.cpp
@@ -6,15 +6,17 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
-
 #include "nna_hw.h"
 #include "nna_interface.h"
 #include "nna_config.h"
-
 #include "ion_alloc.h"
 
 static void* gp_vaddr;
 static void* gp_paddr;
+
+void loadInMem() {
+	
+}
 void myProgram() {
 	printf("Lets goooo!");
 	sunxi_ion_alloc_open();
@@ -22,19 +24,19 @@ void myProgram() {
 	
 	
 
-	uint32_t  buf1_offset = 0x40000; // temp buffer for input/output data
+	uint32_t  data_offset = 0x00000; // data buffer
 	
 	
 	uint32_t data[5] = {5, 2, 3, 4, 5};
-	//printf("\n%p\n", (uint32_t*)((gp_paddr)+buf1_offset));
-	printf("%p\n", (uint32_t)(gp_paddr)+buf1_offset);
+	
+	printf("%p\n", (uint32_t)(gp_paddr)+data_offset);
 	
 	
 	void *ptr;
 	ptr = malloc(sizeof(data));
 	
-	sunxi_ion_loadin(data, sizeof(data), (uint32_t)(gp_paddr)+buf1_offset);
-	sunxi_ion_loadout((uint32_t)(gp_paddr)+buf1_offset, sizeof(data), ptr);
+	sunxi_ion_loadin(data, sizeof(data), (uint32_t)(gp_paddr)+data_offset);
+	sunxi_ion_loadout((uint32_t)(gp_paddr)+data_offset, sizeof(data), ptr);
 	
 	
 	for (int i = 0; i < 5; i++) {
@@ -47,21 +49,13 @@ void myProgram() {
 }
 
 int main(int argc, char **argv) {
-
-  // Set clock to 400Mhz (DDR2 memory speed ??)
   nna_configure(nna_cmd_clk, 400);
-
-  // Turn on NNA
   nna_on();
-
-  // Map NNA registers
   void* r = xreg_open();
   if (r) {
     nna_reset();
 	myProgram();
     xreg_close();
   }
-
   nna_off();
-
 }
